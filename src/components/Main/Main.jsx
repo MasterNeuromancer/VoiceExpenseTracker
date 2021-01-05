eaab71bbaa7010aee2d0d5a7e7f7ed9753e8b13f
Neuromancer93 <itrejonumber1@gmail.com>
2021-01-04 22:11:40 -0800

    created form component and added it to main screen

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Card, CardHeader, CardContent, Typography, Grid, Divider } from '@material-ui/core';
 
 import useStyles from './styles';
+import Form from './Form/Form';
 
 const Main = () => {
     const classes = useStyles();
@@ -10,13 +11,13 @@ const Main = () => {
         <Card className={classes.root}>
             <CardHeader title='Expense Tracker' subheader='Powered by Speechly' />
             <CardContent>
-                <Typography align='center' variant='h5'>
-                    Total Balance $100
-                </Typography>
+                <Typography align='center' variant='h5'>Total Balance $100</Typography>
                 <Typography variant='subtitle1' style={{ lineHeight: '1.5em', marginTop: '20px'}}>
+                    {/* infocard */}
                     Try saying: Add income for $100 in Category Salary for Monday...
                 </Typography>
                 <Divider />
+                <Form />
             </CardContent>
             <CardContent className={classes.cardContent}>
                 <Grid item xs={12}>
